a7c10ddda9c97ebe3a5ec0398add674b73092198
Alexander Brown <DrJKL0424@gmail.com>
2015-05-31 20:34:04 -0700

    Extract creation of Swing Components

--- src/src/Client.java
+++ src/src/Client.java
@@ -12,9 +12,12 @@ import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
@@ -85,12 +88,11 @@ class EnvFrame extends JFrame {
         final Strain dripStrain = new DrippyStrain();
         final Strain floatStrain = new FloatyStrain();
         final Strain rayStrain = new RayStrain();
-        final Strain[] strainOptions = { defStrain, dripStrain, floatStrain, rayStrain };
-
-        for (final Strain s : strainOptions) {
-            final String strName = s.getStrainName();
-            strToStrain.put(strName, s);
-        }
+        final List<Strain> strainOptions = Arrays.asList(defStrain, dripStrain, floatStrain,
+                rayStrain);
+        strainOptions.forEach(s -> {
+            strToStrain.put(s.getStrainName(), s);
+        });
 
         myPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 
@@ -126,77 +128,13 @@ class EnvFrame extends JFrame {
         numUpdatesLabel = new JLabel("Updates: " + envr.updates);
         visStrainLabel = new JLabel(envr.listStrainData());
 
-        newImageButton = new JButton("Reset");
-        newImageButton.addActionListener(e -> {
-            if (myTimer.isRunning()) {
-                myTimer.stop();
-                startStopButton.setText("Start");
-            }
-            if (randTimer.isRunning()) {
-                randTimer.stop();
-                startStopRandomButton.setText("Start Random");
-            }
-            setUpEnvironment();
-            updateData();
-            toggleRandomButtons();
-            toggleRunningButtons();
-            contentPane.add(myPanel, "Center");
-            myPanel.repaint();
-        });
+        createNewImageButton(contentPane);
+        createSetImageButton(contentPane, fc);
+        createStartStopRandomButton();
 
-        setImageButton = new JButton("Set Image");
-        setImageButton.addActionListener(e -> {
-            fc.showOpenDialog(null);
-            final File img = fc.getSelectedFile();
-            setUpEnvironment(img);
-            setSize(envr.width, envr.height);
-            myPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-            updateData();
-            myPanel.repaint();
-            contentPane.add(myPanel, "Center");
-            contentPane.validate();
-        });
+        createStartStopButton();
 
-        startStopRandomButton = new JButton("Start Random");
-        startStopRandomButton.addActionListener(e -> {
-            if (myTimer.isRunning()) {
-                myTimer.stop();
-                startStopButton.setText("Start");
-            }
-            if (randTimer.isRunning()) {
-                randTimer.stop();
-                startStopRandomButton.setText("Start Random");
-            } else {
-                randTimer.start();
-                startStopRandomButton.setText("Pause Random");
-            }
-            toggleRandomButtons();
-            toggleRunningButtons();
-        });
-
-        startStopButton = new JButton("Start");
-        startStopButton.addActionListener(e -> {
-            if (randTimer.isRunning()) {
-                randTimer.stop();
-                startStopRandomButton.setText("Start Random");
-            }
-            if (myTimer.isRunning()) {
-                myTimer.stop();
-                startStopButton.setText("Start");
-            } else {
-                myTimer.start();
-                startStopButton.setText("Pause");
-            }
-            toggleRandomButtons();
-            toggleRunningButtons();
-        });
-
-        singleStepButton = new JButton("Step");
-        singleStepButton.addActionListener(e -> {
-            envr.update();
-            updateData();
-            myPanel.repaint();
-        });
+        createSingleStepButton();
 
         saveImageButton = new JButton("Save Image");
         saveImageButton.addActionListener(e -> envr.saveImage());
@@ -204,94 +142,15 @@ class EnvFrame extends JFrame {
         saveNegButton = new JButton("Save Negative");
         saveNegButton.addActionListener(e -> envr.saveNegative());
 
-        orgAddButton = new JButton("Add Organism");
-        orgAddButton.addActionListener(e -> {
-            final String strChoice = ((src.orgAddPanel) orgAddPanel).getChosenStrain();
-            final Strain str = strToStrain.get(strChoice);
-            envr.add(str);
-            updateData();
-        });
+        createOrgAddButton();
+        createMultOrgAddButton();
+        createOrgPlaceButton();
+        createExterminateStrainButton();
 
-        multOrgAddButton = new JButton("Add Organisms");
-        multOrgAddButton.addActionListener(e -> {
-            final int num = Integer.parseInt(JOptionPane.showInputDialog("Number of Organisms?"));
-            final String strChoice = ((src.orgAddPanel) orgAddPanel).getChosenStrain();
-            final Strain str = strToStrain.get(strChoice);
-            envr.add(num, str);
-            updateData();
-        });
-
-        orgPlaceButton = new JButton("Place Organism");
-        orgPlaceButton.addActionListener(e -> {
-            final String strChoice = ((src.orgAddPanel) orgAddPanel).getChosenStrain();
-            final Strain str = strToStrain.get(strChoice);
-            int placeX = envr.width / 2;
-            int placeY = envr.height / 2;
-            try {
-                final String xValue = JOptionPane.showInputDialog("X? (0-" + envr.width + ")");
-                placeX = Integer.parseInt(xValue);
-            } catch (final NumberFormatException e11) {
-                System.err.println("Failed to get X Value");
-                return;
-            }
-            try {
-                final String yValue = JOptionPane.showInputDialog("Y? (0-" + envr.height + ")");
-                placeY = Integer.parseInt(yValue);
-            } catch (final NumberFormatException e12) {
-                System.err.println("Failed to get Y Value");
-                return;
-            }
-            envr.addOneAt(str, placeY, placeX);
-            updateData();
-        });
-
-        exterminateStrainButton = new JButton("Exterminate Strain");
-        exterminateStrainButton.addActionListener(arg0 -> {
-            final Strain strChoice = (Strain) JOptionPane.showInputDialog(new JFrame(), "Strain?",
-                    "Strain Choice", JOptionPane.PLAIN_MESSAGE, null, envr.activeStrains.keySet()
-                            .toArray(), envr.activeStrains.keySet().toArray()[0]);
-            final String strX = strChoice.getStrainName();
-            envr.exterminate(strToStrain.get(strX));
-            updateData();
-        });
-
-        showLivingData = new JButton("Living Data");
-        showLivingData.addActionListener(e -> {
-            final JTextArea strainText = new JTextArea(envr.listLiving(), 10, 50);
-            final JScrollPane sText = new JScrollPane(strainText);
-            sText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-            JOptionPane.showMessageDialog(null, sText);
-        });
-
-        showTombData = new JButton("Tombed Data");
-        showTombData.addActionListener(e -> {
-            final JTextArea strainText = new JTextArea(envr.listTombed(), 10, 50);
-            final JScrollPane sText = new JScrollPane(strainText);
-            sText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-            JOptionPane.showMessageDialog(null, sText);
-        });
-
-        showStrainData = new JButton("Strain Data");
-        showStrainData.addActionListener(e -> {
-            final JTextArea strainText = new JTextArea(envr.listStrains(), 10, 50);
-            final JScrollPane sText = new JScrollPane(strainText);
-            sText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-            JOptionPane.showMessageDialog(null, sText);
-        });
-
-        showAllStrainData = new JButton("All Strain Data");
-        showAllStrainData.addActionListener(e -> {
-            final JFrame strainDataOptPane = new JFrame();
-            strainDataOptPane.add(strainDataPane);
-            strainDataOptPane.setSize(200, 800);
-            strainDataOptPane.setVisible(true);
-        });
-
-        final JSlider speedBar = new JSlider(1, MAX_DELAY);
-        speedBar.addChangeListener(ce -> {
-            myTimer.setDelay(1000 / (speedBar.getValue()));
-            randTimer.setDelay(1000 / (speedBar.getValue()));
-        });
+        createShowLivingData();
+        createShowTombData();
+        createShowStrainData();
+        createShowAllStrainData(strainDataPane);
 
         numOrgLabel.setAlignmentX((float) 0.5);
         orgAddButton.setAlignmentX((float) 0.5);
@@ -335,9 +194,7 @@ class EnvFrame extends JFrame {
         dataButtons.add(showLivingData);
         dataButtons.add(showTombData);
 
-        mainControlsPanel.add(new JLabel(" Slow"));
-        mainControlsPanel.add(speedBar);
-        mainControlsPanel.add(new JLabel("Fast "));
+        addSpeedSlider();
 
         mainControlsPanel.add(startStopRandomButton);
 
@@ -346,37 +203,212 @@ class EnvFrame extends JFrame {
 
     }
 
+    private void addSpeedSlider() {
+        final JSlider speedBar = new JSlider(1, MAX_DELAY);
+        speedBar.addChangeListener(ce -> {
+            myTimer.setDelay(1000 / (speedBar.getValue()));
+            randTimer.setDelay(1000 / (speedBar.getValue()));
+        });
+
+        mainControlsPanel.add(new JLabel(" Slow"));
+        mainControlsPanel.add(speedBar);
+        mainControlsPanel.add(new JLabel("Fast "));
+    }
+
+    private void createShowAllStrainData(final JTabbedPane strainDataPane) {
+        showAllStrainData = new JButton("All Strain Data");
+        showAllStrainData.addActionListener(e -> {
+            final JFrame strainDataOptPane = new JFrame();
+            strainDataOptPane.add(strainDataPane);
+            strainDataOptPane.setSize(200, 800);
+            strainDataOptPane.setVisible(true);
+        });
+    }
+
+    private void createShowStrainData() {
+        showStrainData = new JButton("Strain Data");
+        showStrainData.addActionListener(e -> {
+            final JTextArea strainText = new JTextArea(envr.listStrains(), 10, 50);
+            final JScrollPane sText = new JScrollPane(strainText);
+            sText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+            JOptionPane.showMessageDialog(null, sText);
+        });
+    }
+
+    private void createShowTombData() {
+        showTombData = new JButton("Tombed Data");
+        showTombData.addActionListener(e -> {
+            final JTextArea strainText = new JTextArea(envr.listTombed(), 10, 50);
+            final JScrollPane sText = new JScrollPane(strainText);
+            sText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+            JOptionPane.showMessageDialog(null, sText);
+        });
+    }
+
+    private void createOrgPlaceButton() {
+        orgPlaceButton = new JButton("Place Organism");
+        orgPlaceButton.addActionListener(e -> {
+            final String strChoice = ((src.orgAddPanel) orgAddPanel).getChosenStrain();
+            final Strain str = strToStrain.get(strChoice);
+            int placeX = envr.width / 2;
+            int placeY = envr.height / 2;
+            try {
+                final String xValue = JOptionPane.showInputDialog("X? (0-" + envr.width + ")");
+                placeX = Integer.parseInt(xValue);
+            } catch (final NumberFormatException e11) {
+                System.err.println("Failed to get X Value");
+                return;
+            }
+            try {
+                final String yValue = JOptionPane.showInputDialog("Y? (0-" + envr.height + ")");
+                placeY = Integer.parseInt(yValue);
+            } catch (final NumberFormatException e12) {
+                System.err.println("Failed to get Y Value");
+                return;
+            }
+            envr.addOneAt(str, placeY, placeX);
+            updateData();
+        });
+    }
+
+    private void createShowLivingData() {
+        showLivingData = new JButton("Living Data");
+        showLivingData.addActionListener(e -> {
+            final JTextArea strainText = new JTextArea(envr.listLiving(), 10, 50);
+            final JScrollPane sText = new JScrollPane(strainText);
+            sText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+            JOptionPane.showMessageDialog(null, sText);
+        });
+    }
+
+    private void createExterminateStrainButton() {
+        exterminateStrainButton = new JButton("Exterminate Strain");
+        exterminateStrainButton.addActionListener(arg0 -> {
+            final Strain strChoice = (Strain) JOptionPane.showInputDialog(new JFrame(), "Strain?",
+                    "Strain Choice", JOptionPane.PLAIN_MESSAGE, null, envr.activeStrains.keySet()
+                            .toArray(), envr.activeStrains.keySet().toArray()[0]);
+            final String strX = strChoice.getStrainName();
+            envr.exterminate(strToStrain.get(strX));
+            updateData();
+        });
+    }
+
+    private void createMultOrgAddButton() {
+        multOrgAddButton = new JButton("Add Organisms");
+        multOrgAddButton.addActionListener(e -> {
+            final int num = Integer.parseInt(JOptionPane.showInputDialog("Number of Organisms?"));
+            final String strChoice = ((src.orgAddPanel) orgAddPanel).getChosenStrain();
+            final Strain str = strToStrain.get(strChoice);
+            envr.add(num, str);
+            updateData();
+        });
+    }
+
+    private void createOrgAddButton() {
+        orgAddButton = new JButton("Add Organism");
+        orgAddButton.addActionListener(e -> {
+            final String strChoice = ((src.orgAddPanel) orgAddPanel).getChosenStrain();
+            final Strain str = strToStrain.get(strChoice);
+            envr.add(str);
+            updateData();
+        });
+    }
+
+    private void createSingleStepButton() {
+        singleStepButton = new JButton("Step");
+        singleStepButton.addActionListener(e -> {
+            envr.update();
+            updateData();
+            myPanel.repaint();
+        });
+    }
+
+    private void createStartStopRandomButton() {
+        startStopRandomButton = new JButton("Start Random");
+        startStopRandomButton.addActionListener(e -> {
+            if (myTimer.isRunning()) {
+                myTimer.stop();
+                startStopButton.setText("Start");
+            }
+            if (randTimer.isRunning()) {
+                randTimer.stop();
+                startStopRandomButton.setText("Start Random");
+            } else {
+                randTimer.start();
+                startStopRandomButton.setText("Pause Random");
+            }
+            toggleRandomButtons();
+            toggleRunningButtons();
+        });
+    }
+
+    private void createSetImageButton(final Container contentPane, final JFileChooser fc) {
+        setImageButton = new JButton("Set Image");
+        setImageButton.addActionListener(e -> {
+            fc.showOpenDialog(null);
+            final File img = fc.getSelectedFile();
+            setUpEnvironment(img);
+            setSize(envr.width, envr.height);
+            myPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+            updateData();
+            myPanel.repaint();
+            contentPane.add(myPanel, "Center");
+            contentPane.validate();
+        });
+    }
+
+    private void createNewImageButton(final Container contentPane) {
+        newImageButton = new JButton("Reset");
+        newImageButton.addActionListener(e -> {
+            if (myTimer.isRunning()) {
+                myTimer.stop();
+                startStopButton.setText("Start");
+            }
+            if (randTimer.isRunning()) {
+                randTimer.stop();
+                startStopRandomButton.setText("Start Random");
+            }
+            setUpEnvironment();
+            updateData();
+            toggleRandomButtons();
+            toggleRunningButtons();
+            contentPane.add(myPanel, "Center");
+            myPanel.repaint();
+        });
+    }
+
+    private void createStartStopButton() {
+        startStopButton = new JButton("Start");
+        startStopButton.addActionListener(e -> {
+            if (randTimer.isRunning()) {
+                randTimer.stop();
+                startStopRandomButton.setText("Start Random");
+            }
+            if (myTimer.isRunning()) {
+                myTimer.stop();
+                startStopButton.setText("Start");
+            } else {
+                myTimer.start();
+                startStopButton.setText("Pause");
+            }
+            toggleRandomButtons();
+            toggleRunningButtons();
+        });
+    }
+
     private void toggleRandomButtons() {
-        if (randTimer.isRunning()) {
-            for (final JButton but : orgButtons) {
-                but.setEnabled(false);
-            }
-            for (final JButton but : dataButtons) {
-                but.setEnabled(false);
-            }
-        } else {
-            for (final JButton but : orgButtons) {
-                but.setEnabled(true);
-            }
-            for (final JButton but : dataButtons) {
-                but.setEnabled(true);
-            }
-        }
+        Stream.concat(orgButtons.stream(), dataButtons.stream()).forEach(button -> {
+            button.setEnabled(!randTimer.isRunning());
+        });
     }
 
     private void toggleRunningButtons() {
         if (randTimer.isRunning()) {
             return;
         }
-        if (myTimer.isRunning()) {
-            for (final JButton but : dataButtons) {
-                but.setEnabled(false);
-            }
-        } else {
-            for (final JButton but : dataButtons) {
-                but.setEnabled(true);
-            }
-        }
+        dataButtons.forEach(button -> {
+            button.setEnabled(!myTimer.isRunning());
+        });
     }
 
     private void setUpEnvironment() {
