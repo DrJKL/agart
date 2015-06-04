36ec0dae21571240433a5b2acab6c9b6a26d2839
Alexander Brown <DrJKL0424@gmail.com>
2015-06-04 08:53:09 -0700

    Simplify OrgAddPanel

--- src/client/OrgAddPanel.java
+++ src/client/OrgAddPanel.java
@@ -6,6 +6,8 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
@@ -14,11 +16,6 @@ import javax.swing.JRadioButton;
 
 @SuppressWarnings("serial")
 class OrgAddPanel extends JPanel implements ActionListener {
-
-  static String defaultStrainString = "Default VIRUS";
-  static String dripStrainString = "Drippy VIRUS";
-  static String floatStrainString = "Floaty VIRUS";
-  static String rayStrainString = "Ray VIRUS";
 
   static String defaultStrain = "Default VIRUS ";
   static String dripStrain = "Drippy VIRUS ";
@@ -31,34 +28,22 @@ class OrgAddPanel extends JPanel implements ActionListener {
 
     super(new BorderLayout());
 
-    final JRadioButton defStrainButton = makeStrainButton(defaultStrain, KeyEvent.VK_1);
-
-    final JRadioButton dripStrainButton = makeStrainButton(dripStrain, KeyEvent.VK_2);
-
-    final JRadioButton floatStrainButton = makeStrainButton(floatStrain, KeyEvent.VK_3);
-
-    final JRadioButton rayStrainButton = makeStrainButton(rayStrain, KeyEvent.VK_4);
-
-    defStrainButton.setSelected(true);
-
-    final ButtonGroup group = new ButtonGroup();
-    group.add(defStrainButton);
-    group.add(dripStrainButton);
-    group.add(floatStrainButton);
-    group.add(rayStrainButton);
-
-    defStrainButton.addActionListener(this);
-    dripStrainButton.addActionListener(this);
-    floatStrainButton.addActionListener(this);
-    rayStrainButton.addActionListener(this);
-
     chosenStrain = defaultStrain;
 
+    final List<JRadioButton> buttons = Arrays.asList(
+        makeStrainButton(defaultStrain, KeyEvent.VK_1),
+        makeStrainButton(dripStrain, KeyEvent.VK_2), //
+        makeStrainButton(floatStrain, KeyEvent.VK_3), //
+        makeStrainButton(rayStrain, KeyEvent.VK_4));
+    buttons.get(0).setSelected(true);
+
+    buttons.forEach(button -> button.addActionListener(this));
+
+    final ButtonGroup group = new ButtonGroup();
+    buttons.forEach(button -> group.add(button));
+
     final JPanel radioPanel = new JPanel(new GridLayout(0, 1));
-    radioPanel.add(defStrainButton);
-    radioPanel.add(dripStrainButton);
-    radioPanel.add(floatStrainButton);
-    radioPanel.add(rayStrainButton);
+    buttons.forEach(button -> radioPanel.add(button));
 
     add(radioPanel, BorderLayout.LINE_START);
     setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
@@ -66,7 +51,6 @@ class OrgAddPanel extends JPanel implements ActionListener {
     final Dimension d = new Dimension();
     d.setSize(5000, 100);
     this.setMaximumSize(d);
-
   }
 
   private static JRadioButton makeStrainButton(String name, int mnemonic) {
